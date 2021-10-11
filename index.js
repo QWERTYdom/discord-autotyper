696635c0c37f3496a889a5177cd14b01fcd90fcd
QWERTYdom <92232579+QWERTYdom@users.noreply.github.com>
2021-10-11 17:34:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { sig } = require("./utils/sig");
 
     await dc.initialize();
     // here is where you enter your email and password
-    await dc.login('email', 'password')
+    await dc.login('findmehfindu@gmail.com', 'Southblue')
 
     await dc.likeChannelProcess('server id', 'channel id', 1) // 1 = 1 minute
 
